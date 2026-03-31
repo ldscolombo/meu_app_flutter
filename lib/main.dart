d4491a725ddc1f6f70fe6c69daf14d5b733c73d7
Seu Nome <seuemail@gmail.com>
2026-03-30 22:03:56 -0300

    aula 30/03 contador

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MeuApp());
+}
+
+class MeuApp extends StatelessWidget{
+  MeuApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: const Color.fromARGB(255, 16, 89, 216),
@@ -10,16 +16,42 @@ void main() {
            child: Text("Nome do meu app"),
           ),
         ),
-        body: Center( 
-         child: Column( 
-          mainAxisAlignment: MainAxisAlignment.center,
-         children: [
-          Text("olá, mundo"),
-          Text("23/03/26")
-         ],
+        body: PaginaInicial()
+    ),
+   );
+  }
+}
+
+class PaginaInicial extends StatefulWidget{
+  @override
+  State<PaginaInicial> createState() => PaginaInicialState();
+}
+
+class PaginaInicialState extends State<PaginaInicial>{
+  String texto = 'olá, mundo!';
+  int contador = 0;
+@override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            texto,
+            style: TextStyle(fontSize: 20),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            child: Text('mudar texto'),
+            onPressed: () {
+              setState(() {
+                contador++;
+                texto = "Você alterou o texto ${++contador} ${contador >1 ? "vezes" : "vez"}";
+              });
+            },
+          )
+        ],
       ),
-     )
-    )
-   )
-  );
+    );
+  }
 }
