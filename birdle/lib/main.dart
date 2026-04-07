711d8ff46dea2e0bb0ec148df39fff940a98b9e1
Seu Nome <seuemail@gmail.com>
2026-04-06 21:44:56 -0300

    segundo passo

--- birdle/lib/main.dart
+++ birdle/lib/main.dart
@@ -7,14 +7,49 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
-  @override
+   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('hi Carlos!'),
+          child: // main.dart line ~16
+// green
+          Tile('A', HitType.hit);
+// grey
+          Tile('A', HitType.miss);
+        // yellow
+          Tile('A', HitType.partial);
+
         ),
       ),
     );
   }
 }
+
+class Tile extends StatelessWidget {
+  const Tile(this.letter, this.hitType, {super.key});
+
+  final String letter;
+  final HitType hitType;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 60,
+      height: 60,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade300),
+        color: switch (hitType) {
+          HitType.hit => Colors.green,
+          HitType.partial => Colors.yellow,
+          HitType.miss => Colors.grey,
+          _ => Colors.white,
+        },
+        // TODO: add children
+      ),
+    );
+  }
+}
+
+
+
